bc2822e045051b84e30b19f93bca40f01efd44cd
IvanRizal <ivanrizal36@gmail.com>
2025-06-11 21:57:24 +0700

    landing page modifikasi

--- resources/views/landingpage/app.blade.php
+++ resources/views/landingpage/app.blade.php
@@ -31,28 +31,9 @@
         <div class="row py-2 px-lg-5">
             <div class="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
                 <div class="d-inline-flex align-items-center text-white">
-                    <small><i class="fa fa-phone-alt mr-2"></i>+012 345 6789</small>
+                    <small><i class="fa fa-phone-alt mr-2"></i>0341-404424, 404425</small>
                     <small class="px-3">|</small>
-                    <small><i class="fa fa-envelope mr-2"></i>info@example.com</small>
-                </div>
-            </div>
-            <div class="col-lg-6 text-center text-lg-right">
-                <div class="d-inline-flex align-items-center">
-                    <a class="text-white px-2" href="">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a class="text-white px-2" href="">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a class="text-white px-2" href="">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                    <a class="text-white px-2" href="">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a class="text-white pl-2" href="">
-                        <i class="fab fa-youtube"></i>
-                    </a>
+                    <small><i class="fa fa-envelope mr-2"></i>info@polinema.ac.id</small>
                 </div>
             </div>
         </div>
@@ -70,23 +51,9 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
-                <div class="navbar-nav mx-auto py-0">
-                    <a href="{{ url('/') }}" class="nav-item nav-link active">Home</a>
-                    <a href="about.html" class="nav-item nav-link">About</a>
-                    <a href="course.html" class="nav-item nav-link">Courses</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                        <div class="dropdown-menu m-0">
-                            <a href="detail.html" class="dropdown-item">Course Detail</a>
-                            <a href="feature.html" class="dropdown-item">Our Features</a>
-                            <a href="team.html" class="dropdown-item">Instructors</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        </div>
-                    </div>
-                    <a href="contact.html" class="nav-item nav-link">Contact</a>
-                </div>
-                <a href="{{ route('login') }}" class="btn btn-primary py-2 px-4 d-none d-lg-block">Login</a>
-            </div>
+    <a href="{{ route('login') }}" class="btn btn-primary py-2 px-4 ml-auto d-none d-lg-block">Login</a>
+</div>
+
         </nav>
     </div>
     <!-- Navbar End -->
@@ -99,23 +66,17 @@
             <div class="row">
                 <div class="col-md-6 mb-5">
                     <a href="{{ url('') }}" class="navbar-brand">
-                        <h1 class="mt-n2 text-uppercase text-white"><i class="fa fa-book-reader mr-3"></i>Edukate</h1>
+                        <h1 class="mt-n2 text-uppercase text-white"><i class="fa fa-book-reader mr-3"></i>POLINEMA TOEIC</h1>
                     </a>
                     <p class="m-0">Polinema TOEIC Registration System An integrated platform to support students in registering and preparing for the TOEIC exam. Managed by the Language Center (UPA Bahasa), this system ensures a smooth and reliable experience — from registration to certification.</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4 mb-5">
-                    <h3 class="text-white mb-4">Get In Touch</h3>
+                    <h3 class="text-white mb-4">Contact & Address</h3>
                     <p><i class="fa fa-map-marker-alt mr-2"></i>Jl. Soekarno Hatta No. 9, Malang,East Java - Indonesia</p>
-                    <p><i class="fa fa-phone-alt mr-2"></i>0896-0900-4444</p>
-                    <p><i class="fa fa-envelope mr-2"></i>Email: plldir@polinema.ac.id</p>
-                    <div class="d-flex justify-content-start mt-4">
-                        <a class="text-white mr-4" href="#"><i class="fab fa-2x fa-twitter"></i></a>
-                        <a class="text-white mr-4" href="#"><i class="fab fa-2x fa-facebook-f"></i></a>
-                        <a class="text-white mr-4" href="#"><i class="fab fa-2x fa-linkedin-in"></i></a>
-                        <a class="text-white" href="#"><i class="fab fa-2x fa-instagram"></i></a>
-                    </div>
+                    <p><i class="fa fa-phone-alt mr-2"></i>0341-404424, 404425</p>
+                    <p><i class="fa fa-envelope mr-2"></i>Email: info@polinema.ac.id</p>
                 </div>
                 <div class="col-md-4 mb-5">
                     <h3 class="text-white mb-4">Departements At Polinema</h3>
